648ef7a1c95c017741b472736c8e404fd9580813
Sugiyama Ryo <sugisugirr@gmail.com>
2016-09-16 15:39:17 +0900

    課題１終わり

---
+++ Sugiyama/sailing-kadai/sailing-kadai/Array1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void main(){
+
+	/* •Ï”éŒ¾ */
+
+	int score[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	int i;
+
+	/* ˆ— */
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\n", score[i]);
+	}
+
+	return;
+}
