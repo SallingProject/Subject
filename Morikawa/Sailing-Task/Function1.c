380bd0bec641420731689ac83885ef32a31f8c27
森川 皓斗 <take02hiarts@gmail.com>
2016-09-18 13:58:25 +0900

    Sailing課題 Function2 再提出1

--- Morikawa/Sailing-Task/Function1.c
+++ Morikawa/Sailing-Task/Function1.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
 
-int change(int);
+void change(int);
 
 void main()
 {
 	int num = 0;
 
 	printf("初期化前：%d\n", num);
-
-	num = change(num);
-
-	printf("初期化後：%d\n", num);
+	change(num);
 
 	return;
 }
 
-int change(int num)
+void change(int num)
 {
 	num = 10;
-
-	return num;
+	printf("初期化後：%d\n", num);
 }
