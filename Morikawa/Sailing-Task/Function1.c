331a471b3ad32ff297b6dd120caf4a75b1976d1c
森川 皓斗 <take02hiarts@gmail.com>
2016-09-18 13:42:40 +0900

    Sailing課題 Function1

---
+++ Morikawa/Sailing-Task/Function1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int change(int);
+
+void main()
+{
+	int num = 0;
+
+	printf("‰Šú‰»‘OF%d\n", num);
+
+	num = change(num);
+
+	printf("‰Šú‰»ŒãF%d\n", num);
+
+	return;
+}
+
+int change(int num)
+{
+	num = 10;
+
+	return num;
+}
