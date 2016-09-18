13d7c2e9985f1b745e8d8edba4251cd89a6e54e8
森川 皓斗 <take02hiarts@gmail.com>
2016-09-18 21:46:47 +0900

    Sailing課題 Function1 治し二回目

--- Morikawa/Sailing-Task/Function1.c
+++ Morikawa/Sailing-Task/Function1.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 
+#define MIN 0
+#define MAX 10
+
 void change(int);
 
 void main()
 {
-	int num = 0;
+	int num = MIN;
 
 	printf("初期化前：%d\n", num);
-	change(num);
+
+	change(&num);
+
+	printf("初期化後：%d\n", num);
 
 	return;
 }
 
-void change(int num)
+void change(int *num)
 {
-	num = 10;
-	printf("初期化後：%d\n", num);
+	*num = MAX;
 }
