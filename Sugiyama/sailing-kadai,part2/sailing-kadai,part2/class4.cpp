3b4cabcf55fc485edb097cc1f8334387d8454b75
youtsuumochii <sugisugirr@gmail.com>
2016-09-26 16:09:27 +0900

    class4提出

---
+++ Sugiyama/sailing-kadai,part2/sailing-kadai,part2/class4.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+class STATUS {
+private:
+	int m_HP;
+	int m_AT;
+	int m_DF;
+public:
+	STATUS(int, int, int);
+	void Disp();
+};
+
+STATUS::STATUS(int c_HP, int c_AT, int c_DF)
+{
+	m_HP = c_HP;
+	m_AT = c_AT;
+	m_DF = c_DF;
+}
+
+void STATUS::Disp()
+{
+	cout << "HP: " << m_HP << endl;
+	cout << "AT: " << m_AT << endl;
+	cout << "DF: " << m_DF << endl;
+}
+
+void main() {
+
+
+	STATUS status(10, 7, 4);
+
+	status.Disp();
+
+
+	return;
+}
