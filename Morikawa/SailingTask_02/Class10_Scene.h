cdf0b80aeb8ca04a989ad14b13601ee9def3fd39
Hiroto Morikawa <take02hiarts@gmail.com>
2016-09-30 23:35:31 +0900

    課題2-10提出

---
+++ Morikawa/SailingTask_02/Class10_Scene.h
@@ -0,0 +1,18 @@
+#ifndef _SCENE_H_
+#define _SCENE_H_
+
+/************************************
+ 全てのシーンの継承元となる抽象クラス
+ ************************************/
+
+class Scene {
+
+public:
+	virtual void Start() = 0;    // 処理の開始
+
+	virtual void Initialize() {} // 初期化処理
+	virtual void Input() {}      // 入力処理
+	virtual void Output() {}     // 出力処理
+};
+
+#endif // _SCENE_H_
---
+++ Morikawa/SailingTask_02/Class10_main.cpp
@@ -0,0 +1,17 @@
+#include "Class10_Scene.h"
+#include "Class10_Title.h"
+#include "Class10_Game.h"
+
+void main() {
+
+	Title title;
+	Game game;
+
+	while (1) {
+
+		title.Start();
+		game.Start();
+	}
+
+	return;
+}
