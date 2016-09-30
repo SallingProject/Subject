589e10c3b56961e5d83fd0f13596783df6990a6e
Hiroto Morikawa <take02hiarts@gmail.com>
2016-09-30 23:06:20 +0900

    課題2-9提出

--- Morikawa/SailingTask_02/Class9.cpp
+++ Morikawa/SailingTask_02/Class9.cpp
@@ -1,39 +1,39 @@
-#include <iostream>
-
-using namespace std;
-
-class SceneBase {
-public:
-	virtual void update() = 0;     // 更新処理
-	virtual void render() = 0;     // 描画処理 
-	virtual void initialize() = 0; // 初期化
-};
-
-class SceneGame : public SceneBase {
-public:
-	void update() {        // 更新処理
-		cout << "update" << endl;
-	}
-	void render() {        // 描画処理
-		cout << "render" << endl;
-	}
-	void initialize() {    // 初期化
-		cout << "initialize" << endl;
+#include <iostream> 
+ 
+using namespace std; 
+ 
+class SceneBase { 
+public:  
+	virtual void update() = 0;     // 更新処理 
+	virtual void render() = 0;     // 描画処理  
+	virtual void initialize() = 0; // 初期化 
+}; 
+ 
+class SceneGame : public SceneBase { 
+public: 
+	void update() {        // 更新処理 
+		cout << "update" << endl; 
 	} 
-};
-
-void main() {
-	SceneBase* pBase;
-	SceneGame* pGame = new SceneGame;
-
-	// アップキャストを行う
-	pBase = pGame;
-
-	SceneGame Game;
-
-	Game.update();
-	Game.render();
-	Game.initialize();
-
-	return;
-}
+	void render() {        // 描画処理 
+		cout << "render" << endl; 
+	} 
+	void initialize() {    // 初期化 
+		cout << "initialize" << endl; 
+	}  
+}; 
+ 
+void main() { 
+	SceneBase* pBase; 
+	SceneGame* pGame = new SceneGame; 
+	 
+	// アップキャストを行う 
+	pBase = pGame; 
+	 
+	SceneGame Game; 
+	 
+	Game.update(); 
+	Game.render(); 
+	Game.initialize(); 
+	 
+	return; 
+} 
